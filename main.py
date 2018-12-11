fb8066db5a6ab8ac3b8c9dc176bcb843d23acbe3
StevenWongg <45689596+SKWangg@users.noreply.github.com>
2018-12-11 16:24:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,22 +19,22 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_
 rf = RandomForestClassifier(n_estimators=100, oob_score=True, random_state=2018)
 rf.fit(x_train, y_train)
 rf_acc = rf.score(x_test, y_test)
-print("RandomForestClassifier Acc: %f", rf_acc)
+print("RandomForestClassifier Acc: ", rf_acc)
 
 # GBDT
 gb = GradientBoostingClassifier(random_state=2018)
 gb.fit(x_train, y_train)
 gb_acc = gb.score(x_test, y_test)
-print("GradientBoostingClassifier Acc: %f", gb_acc)
+print("GradientBoostingClassifier Acc: ", gb_acc)
 
 # XGBoost
 xgb = XGBClassifier(random_state=2018)
 xgb.fit(x_train, y_train)
 xgb_acc = xgb.score(x_test, y_test)
-print("XGBClassifier Acc: %f", xgb_acc)
+print("XGBClassifier Acc: ", xgb_acc)
 
 # LightGBM
 lg = LGBMClassifier(random_state=2018)
 lg.fit(x_train, y_train)
 lg_acc = lg.score(x_test, y_test)
-print("LGBMClassifier Acc: %f", lg_acc)
+print("LGBMClassifier Acc: ", lg_acc)
